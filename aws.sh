c81c599b8bd60e7bbdbd4ae7768036f058d9bc8c
Taylor Carpenter <taylor@codecafe.com>
2012-05-01 22:20:53 -0500

    base and chef client setup

--- aws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -x
-#REMOTE_SCRIPT="http://codecafe.com/chef/ec2_client_setup.sh"
-REMOTE_SCRIPT="http://git.io/ec2chefclient.1c"
-wget -nv -O - "$REMOTE_SCRIPT" | bash
-set +x
-true
--- aws_chef_data.sh
+++ aws_chef_data.sh
@@ -4,5 +4,10 @@ set -e -x
 echo "Setting up base system"
 REMOTE_SCRIPT="https://raw.github.com/gist/2573056/74237aada624251de7ddfe5e8b98a6e39f736e43/base_setup.sh"
 wget -nv -O - "$REMOTE_SCRIPT" | bash
+
+echo "Setting up Chef"
+#REMOTE_SCRIPT="http://codecafe.com/chef/ec2_client_setup.sh"
+REMOTE_SCRIPT="http://git.io/ec2chefclient.1c"
+wget -nv -O - "$REMOTE_SCRIPT" | bash
 set +x
 true
