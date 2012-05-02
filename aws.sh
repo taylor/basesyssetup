507ee05a6f4e615e8a4a4e05ff4f98ed0adf5629
Taylor Carpenter <taylor@codecafe.com>
2012-05-01 22:06:33 -0500

    new upstream url

--- aws.sh
+++ aws.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 #REMOTE_SCRIPT="http://codecafe.com/chef/ec2_client_setup.sh"
-REMOTE_SCRIPT="http://git.io/ec2chefclient.1b"
+REMOTE_SCRIPT="http://git.io/ec2chefclient.1c"
 wget -nv -O - "$REMOTE_SCRIPT" | bash
 set +x
 true
--- aws_data.sh
+++ aws_data.sh
@@ -3,7 +3,7 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 set -e -x
 #REMOTE_SCRIPT="http://codecafe.com/chef/ec2_client_setup.sh"
-REMOTE_SCRIPT="http://git.io/ec2chefclient.1b"
+REMOTE_SCRIPT="http://git.io/ec2chefclient.1c"
 wget -nv -O - "$REMOTE_SCRIPT" | bash
 set +x
 true
