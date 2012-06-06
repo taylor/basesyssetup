dd6761ab779e9ad5669a47020d5d2a0eca13ba22
Taylor Carpenter <taylor@codecafe.com>
2012-06-06 18:37:57 -0500

    package type

--- usb/install_packages.sh
+++ usb/install_packages.sh
@@ -14,14 +14,23 @@ which rpm > /dev/null 2>&1
 if [ "$?" = "0" ] ; then
   RPM_SYS=1
   pmapp=yum
+  linuxflavor=centos
 else
   APT_SYS=1
   pmapp=apt-get
+  linuxflavor=debian
 fi
 
 if [ "$RPM_SYS" ] ; then
+  EPELURL="http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm"
   pushd /tmp
-  wget http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-5.noarch.rpm
+  which wget > /dev/null 2>&1
+  if [ "$?" = "0" ] ; then
+    wget $EPELURL
+  else
+    curl -o epel-release-6-5.noarch.rpm $EPELURL
+  fi
+
   rpm -Uvh epel-release-6-5.noarch.rpm
   popd
 fi
@@ -29,14 +38,14 @@ fi
 #FIXME: package file list for dist
 
 PACKAGE_NAME_LIST=""
-PACKAGE_LIST_FILE=${confdir}/packages-common.list
+PACKAGE_LIST_FILE=${confdir}/${linuxflavor}/packages-common.list
 PACKAGE_NAME_LIST=$(cat $PACKAGE_LIST_FILE | grep -v -e "^#" | cut -f1 -d' ')
 
 $pmapp -y install ${PACKAGE_NAME_LIST}
 
 # package names to be installed
 PACKAGE_NAME_LIST=""
-PACKAGE_LIST_FILE=${confdir}/packages-$sysrole.list
+PACKAGE_LIST_FILE=${confdir}/${linuxflavor}/packages-$sysrole.list
 
 PACKAGE_NAME_LIST=$(cat $PACKAGE_LIST_FILE | grep -v -e "^#" | cut -f1 -d' ')
 
