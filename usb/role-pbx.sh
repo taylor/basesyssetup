e003ec5312b8d83a91728f7ad3804c41966b1033
Taylor Carpenter <taylor@codecafe.com>
2012-06-07 03:49:08 -0500

    installers

--- usb/role-pbx.sh
+++ usb/role-pbx.sh
@@ -2,14 +2,20 @@
 
 mydir=`dirname $0`
 confdir="${mydir}/../conf"
+installers="${mydir}/../installers"
 
 sysrole="$1"
 
-bash ${mydir}/install-rvm.sh
 
-#NOTE: asterisk moved to ../postinstall/
+#NOTE: asterisk moved to ../installers
+mkdir -p /root/installers
+for installer in rvm asterisk adhearsion polycomtftp fax
+do
+  cp "${installers}/install-${installer}.sh" /root/installers
+done
+
+#bash ${mydir}/install-rvm.sh
 #bash ${mydir}/install-asterisk.sh
-
 #bash ${mydir}/install-adhearsion.sh
 #bash ${mydir}/install-polycomtftp.sh
 #bash ${mydir}/install-fax.sh
